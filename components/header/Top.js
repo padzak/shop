a626578baa2cc59574151188c10d30968b6a3ee2
padzak <mrcn.pajak@gmail.com>
2023-05-14 17:45:36 +0200

    Added more elements to page header

--- components/header/Top.js
+++ components/header/Top.js
@@ -1,5 +1,9 @@
 import React from "react";
 import styles from "./styles.module.scss";
+import { MdSecurity } from "react-icons/md";
+import { BsSuitHeart } from "react-icons/bs";
+import { RiAccountPinCircleLine } from "react-icons/ri";
+import { RiArrowDropDownFill } from "react-icons/ri";
 
 export default function Top() {
     return (
@@ -8,7 +12,32 @@ export default function Top() {
                 <div></div>
                 <ul className={styles.top__list}>
                     <li>
-
+                        <img 
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Flag_of_Europe.svg/240px-Flag_of_Europe.svg.png"
+                            alt=""
+                        />
+                        <span>English / EUR</span>
+                    </li>
+                    <li>
+                        <MdSecurity />
+                        <span>Buyer Protection</span>
+                    </li>
+                    <li>
+                        <span>Customer Service</span>
+                    </li>
+                    <li>
+                        <span>Help</span>
+                    </li>
+                    <li>
+                        <BsSuitHeart />
+                        <span>Wishlist</span>
+                    </li>
+                    <li>
+                        <div className={styles.flex}>
+                            <RiAccountPinCircleLine />
+                            <span>Account</span>
+                            <RiArrowDropDownFill />
+                        </div>
                     </li>
                 </ul>
             </div>
