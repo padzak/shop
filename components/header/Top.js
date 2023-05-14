b85fd7ab8c85f47540dc0583f8e5f77ee9210fb4
padzak <mrcn.pajak@gmail.com>
2023-05-14 17:07:50 +0200

    Added Top component for header and react icons package

---
+++ components/header/Top.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+export default function Top() {
+    return (
+        <div className={styles.top}>
+            <div className={styles.top__container}>
+                <div></div>
+                <ul className={styles.top__list}>
+                    <li>
+
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
