846eda1bc45ca59553a609610b5367ff94e43628
padzak <mrcn.pajak@gmail.com>
2023-05-20 19:06:47 +0200

    added socials to project's footer

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import Links from "./Links";
+import Socials from "./Socials";
 
 export default function Footer() {
     return (
         <footer className={styles.footer}>
             <div className={styles.footer__container}>
                 <Links />
+                <Socials />
             </div>
         </footer>
     )
