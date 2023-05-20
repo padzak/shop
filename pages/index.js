b7cfd1c06bfaf37daa10db044a3f96919dedfdbb
padzak <mrcn.pajak@gmail.com>
2023-05-21 00:14:01 +0200

    Commented out ipregistry api access and replaced with static data

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,13 @@ import axios from 'axios';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home({ country }) {
-  console.log(country);
+export default function Home() {
+  
+  let country = {
+    name: 'Poland',
+    flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Flag_of_Europe.svg/240px-Flag_of_Europe.svg.png'
+  }
+  
   return (<div>
     <Header country={country}/>
     <Footer country={country}/>
@@ -16,18 +21,28 @@ export default function Home({ country }) {
   )
 }
 
-export async function getServerSideProps() {
-  let data = await axios
-  .get('https://api.ipregistry.co/66.165.2.7?key=yfq1uw24gda2l0fv')
-  .then((res) =>{
-    return res.data.location.country;
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-  return  {
-    props: {
-      country: { name: data.name, flag: data.flag.emojitwo },
-    },
-  }
-}
+// Below code includes polling location from ipregistry API and passing it to Home  
+
+// export default function Home({ country }) {
+//   return (<div>
+//     <Header country={country}/>
+//     <Footer country={country}/>
+//   </div>
+//   )
+// }
+
+// export async function getServerSideProps() {
+//   let data = await axios
+//   .get('https://api.ipregistry.co/66.165.2.7?key=yfq1uw24gda2l0fv')
+//   .then((res) =>{
+//     return res.data.location.country;
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+//   return  {
+//     props: {
+//       country: { name: data.name, flag: data.flag.emojitwo },
+//     },
+//   }
+// }
