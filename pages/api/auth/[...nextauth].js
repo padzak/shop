9798f81d59fe8bee08d1b7071aa8b5e17491c3fd
padzak <mrcn.pajak@gmail.com>
2023-05-21 16:07:17 +0200

    Added Auth0 authentication

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,8 +2,9 @@ import NextAuth from 'next-auth';
 import AppleProvider from 'next-auth/providers/apple';
 import FacebookProvider from 'next-auth/providers/facebook';
 import GoogleProvider from 'next-auth/providers/google';
-import GithubProvider from 'next-auth/providers/github';
-import TwitterProvider from 'next-auth/providers/twitter';
+// import TwitterProvider from 'next-auth/providers/twitter';
+// import GithubProvider from 'next-auth/providers/github';
+import Auth0Provider from "next-auth/providers/auth0";
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import clientPromise from './lib/mongodb';
 
@@ -23,14 +24,19 @@ export default NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     }),
-    TwitterProvider({ // Twitter developer portal
-      clientId: process.env.TWITTER_ID,
-      clientSecret: process.env.TWITTER_SECRET
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
-    }),
+    // TwitterProvider({ // Visit Twitter developer portal to configure
+    //   clientId: process.env.TWITTER_ID,
+    //   clientSecret: process.env.TWITTER_SECRET
+    // }),
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET
+    // }),
+    Auth0Provider({
+      clientId: process.env.AUTH0_ID,
+      clientSecret: process.env.AUTH0_SECRET,
+      issuer: process.env.AUTH0_ISSUER,
+    })
   ],
   pages: {
     // signIn: '/signin',
