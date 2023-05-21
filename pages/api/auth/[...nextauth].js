3cd0923c0a8ff0651b1dfe3c6b41a64de517cbc8
padzak <mrcn.pajak@gmail.com>
2023-05-21 11:57:11 +0200

    Added mongodb handling for next-auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,12 @@
-import NextAuth from 'next-auth'
-import AppleProvider from 'next-auth/providers/apple'
-import FacebookProvider from 'next-auth/providers/facebook'
-import GoogleProvider from 'next-auth/providers/google'
+import NextAuth from 'next-auth';
+import AppleProvider from 'next-auth/providers/apple';
+import FacebookProvider from 'next-auth/providers/facebook';
+import GoogleProvider from 'next-auth/providers/google';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import clientPromise from './lib/mongodb';
 
 export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     // OAuth authentication providers...
     AppleProvider({
@@ -18,5 +21,12 @@ export default NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     }),
-  ]
-})
+  ],
+  pages: {
+    signIn: '/signin',
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.JWT_SECRET,
+});
