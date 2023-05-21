8c4515ec463b925cd892179e66451d018abd6bbe
padzak <mrcn.pajak@gmail.com>
2023-05-21 19:42:13 +0200

    Added login component and Formik

--- pages/signin.js
+++ pages/signin.js
@@ -3,6 +3,8 @@ import Header from '@/components/header';
 import styles from '@/styles/signin.module.scss';
 import { BiLeftArrowAlt } from 'react-icons/bi';
 import Link from 'next/link';
+import { Formik, Form } from 'formik';
+import LoginInput from '@/components/inputs/loginInput';
 
 export default function signin({ country }) {
     return (
@@ -17,6 +19,21 @@ export default function signin({ country }) {
                         <span>
                             Join us! <Link href="">Go to Store</Link>
                         </span>
+                    </div>
+                    <div className={styles.login__form}>
+                        <h1>Sign in</h1>
+                        <p>
+                            Get full access to our shopping platform. 
+                        </p>
+                        <Formik>
+                            {
+                                (form) => (
+                                    <Form>
+                                        <LoginInput icon="email" placeholder="Email Address" />
+                                    </Form>
+                                )
+                            }
+                        </Formik>
                     </div>
                 </div>
             </div>
