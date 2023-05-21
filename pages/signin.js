ce5ebbd86636704f1484b4a8af4f82fc9c7632af
padzak <mrcn.pajak@gmail.com>
2023-05-21 18:58:20 +0200

    Added sign in page

--- pages/signin.js
+++ pages/signin.js
@@ -1,7 +1,26 @@
-import React from "react";
+import Footer from '@/components/footer';
+import Header from '@/components/header';
+import styles from '@/styles/signin.module.scss';
+import { BiLeftArrowAlt } from 'react-icons/bi';
+import Link from 'next/link';
 
-export default function signin() {
+export default function signin({ country }) {
     return (
-        <div></div>
+        <>
+            <Header country="Poland"/>
+            <div className={styles.login}>
+                <div className={styles.login__container}>
+                    <div className={styles.login__header}>
+                        <div className={styles.back__svg}>
+                            <BiLeftArrowAlt />
+                        </div>
+                        <span>
+                            Join us! <Link href="">Go to Store</Link>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <Footer country="Poland"/>
+        </>
     );
 }
