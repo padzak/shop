35575a64ba830e4b730900b1f0efe4867e9d661a
padzak <mrcn.pajak@gmail.com>
2023-07-10 21:45:58 +0200

    Fixed swiper layout issues

--- components/home/main/index.js
+++ components/home/main/index.js
@@ -7,7 +7,7 @@ export default function Main() {
         <div className={styles.header}>header</div>
         <div className={styles.menu}>menu</div>
         <div className={styles.swiper}>
-          {/* <MainSwiper /> */}
+          <MainSwiper />
         </div>
         <div className={styles.offers}>offers</div>
         <div className={styles.user}>user</div>
