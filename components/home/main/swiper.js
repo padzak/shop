97c1a2da445501a9585dabec04569fe2c00399a0
padzak <mrcn.pajak@gmail.com>
2023-07-22 18:34:59 +0200

    Solved no flex display for swiper and added some images

--- components/home/main/swiper.js
+++ components/home/main/swiper.js
@@ -13,6 +13,12 @@ import 'swiper/css/navigation';
 import { Pagination, Navigation } from 'swiper/modules';
 
 export default function MainSwiper() {
+  const content = [
+    "https://www.wordstream.com/wp-content/uploads/2021/07/persuasive-ads-coca-cola-1.jpg",
+    "https://smartclip.tv/wp-content/uploads/2021/02/twitter-ad-format-gallery-2.jpg",
+    "https://atomoje.pl/userdata/public/assets//pasek.png",
+    "https://v.wpimg.pl/QUJDREVGfjQrJiR2eTxzIWh-cCw_ZX13P2ZoZ3lxY2EyazQsOiIjMDorfCIkMiE0PTR8NTpoMCUjayR0eSM4JjooMzx5Ijw3LyB9JmUmZ2d8ImRoMCFlZ2dwYnZmajNteCV_czN3NGx5IWAnZHFgdzc",
+  ];
   return (
     <>
       <Swiper
@@ -26,15 +32,13 @@ export default function MainSwiper() {
         modules={[Pagination, Navigation]}
         className="mainSwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        {[...Array(4).keys()].map((i) =>(
+            // eslint-disable-next-line react/jsx-key
+            <SwiperSlide>
+              <img src={content[i]} />
+            
+            </SwiperSlide>
+          ))}
       </Swiper>
     </>
   );
