22fc7ee6ddf9d3c52f15ea41702fa8d66841ccf8
padzak <mrcn.pajak@gmail.com>
2023-07-23 19:02:09 +0200

    Added price and dicount to offers slides

--- components/home/main/offers.js
+++ components/home/main/offers.js
@@ -32,6 +32,8 @@ export default function Offers() {
             <Link href="">
               <img src={offer.image} alt=""/>
             </Link>
+            <span>{offer.price}</span>
+            <span>-{offer.discount}%</span>
           </SwiperSlide>
         )) }
       </Swiper>
