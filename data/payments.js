37133e0628a4529da35ffc5619023de23e56fbc9
padzak <mrcn.pajak@gmail.com>
2023-07-26 09:51:08 +0200

    Added object for payment methods data

---
+++ data/payments.js
@@ -0,0 +1,5 @@
+export const paymentMethodIcons = {
+    visa: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Visa_2021.svg/640px-Visa_2021.svg.png",
+    masterCard: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Mastercard-logo.svg/1544px-Mastercard-logo.svg.png",
+    blik: "https://www.telepolis.pl/images/articles/motorla-moto-z3-play/logo_blik.png",
+}
