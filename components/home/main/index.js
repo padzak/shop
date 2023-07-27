81e5b5555830e60cc07f3915d19a95a17863be67
padzak <mrcn.pajak@gmail.com>
2023-07-27 11:10:05 +0200

    Started writing Header component for main page

--- components/home/main/index.js
+++ components/home/main/index.js
@@ -1,13 +1,15 @@
+import Header from './Header';
 import Menu from './Menu';
 import Offers from './Offers';
 import styles from './styles.module.scss'
 import MainSwiper from './Swiper';
 import User from './User';
+import Link from 'next/link';
 
 export default function Main() {
     return (
       <div className={styles.main}>
-        <div className={styles.header}>header</div>
+        <Header />
         <Menu />
         <div className={styles.swiper}>
           <MainSwiper />
