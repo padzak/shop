ee49e8d6c33832e9c60dd0a22d5a4dedf52b80ff
padzak <mrcn.pajak@gmail.com>
2023-07-30 17:43:24 +0200

    Added countdown to flashdeals and started styling

--- components/home/flashDeals/index.js
+++ components/home/flashDeals/index.js
@@ -1,9 +1,14 @@
+import Countdown from "@/components/countdown";
 import styles from "./styles.module.scss";
+import { MdFlashOn } from "react-icons/md";
 
 export default function FlashDeals() {
     return (
         <div className={styles.flashDeals}>
-            
+            <div className={styles.flashDeals__header}>
+                <h1>FLASH SALE <MdFlashOn /></h1>
+                <Countdown />
+            </div>
         </div>
     );
 }
