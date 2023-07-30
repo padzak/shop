3224e224a80afe7eb2589a68459d7ca56a2f13f5
padzak <mrcn.pajak@gmail.com>
2023-07-30 18:00:22 +0200

    Added flash cards to swiper with initial styling

--- components/home/flashDeals/index.js
+++ components/home/flashDeals/index.js
@@ -9,6 +9,8 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 // import required modules
 import { Navigation } from 'swiper/modules';
+import { flashDealsArray } from "@/data/home";
+import FlashCard from "./FlashCard";
 
 export default function FlashDeals() {
     return (
@@ -24,15 +26,13 @@ export default function FlashDeals() {
                 modules={[Navigation]}
                 className="flashDealsSwiper"
             >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide>
+                <div className={styles.flashDeals__list}>
+                    {flashDealsArray.map((item, i) => (
+                        <SwiperSlide>
+                            <FlashCard product={item} key={i} />
+                        </SwiperSlide>
+                    ))}
+                </div>
             </Swiper>
         </div>
     );
