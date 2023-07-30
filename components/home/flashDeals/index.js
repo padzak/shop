61d1b48277f476858af634be13ac8503821b8a6a
padzak <mrcn.pajak@gmail.com>
2023-07-30 17:52:39 +0200

    Added flash deals swiper

--- components/home/flashDeals/index.js
+++ components/home/flashDeals/index.js
@@ -1,6 +1,14 @@
 import Countdown from "@/components/countdown";
 import styles from "./styles.module.scss";
 import { MdFlashOn } from "react-icons/md";
+import { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+// import required modules
+import { Navigation } from 'swiper/modules';
 
 export default function FlashDeals() {
     return (
@@ -9,6 +17,23 @@ export default function FlashDeals() {
                 <h1>FLASH SALE <MdFlashOn /></h1>
                 <Countdown />
             </div>
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                navigation={true}
+                modules={[Navigation]}
+                className="flashDealsSwiper"
+            >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 5</SwiperSlide>
+                <SwiperSlide>Slide 6</SwiperSlide>
+                <SwiperSlide>Slide 7</SwiperSlide>
+                <SwiperSlide>Slide 8</SwiperSlide>
+                <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper>
         </div>
     );
 }
