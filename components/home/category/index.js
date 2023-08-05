db29624f8bf45af86bf9a70b6ae92aecdaed6cb7
padzak <mrcn.pajak@gmail.com>
2023-08-05 17:16:37 +0200

    Added responsivness to categories for medium screens

--- components/home/category/index.js
+++ components/home/category/index.js
@@ -1,7 +1,9 @@
 import styles from "./styles.module.scss";
 import { BsArrowRightCircle } from 'react-icons/bs';
+import { useMediaQuery } from "react-responsive";
 
 export default function Category({ header, products, background }) {
+    const isMedium = useMediaQuery({ query: "(max-width: 1300px)" });
     return (
         <div className={styles.category} style={{ background: `${background}`}}>
             <div className={styles.category__header}>
@@ -10,7 +12,7 @@ export default function Category({ header, products, background }) {
             </div>
             <div className={styles.category__products}>
                 {
-                    products.map((product, index) => (
+                    products.slice(0, isMedium ? 4 : 6).map((product, index) => (
                         // eslint-disable-next-line react/jsx-key
                         <div className={styles.product}>
                             <img src={product.image} /*alt={product.name}*/ alt="" />
