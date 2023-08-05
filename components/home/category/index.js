2ba0412e4a024568ba7e67076f63999fde910b6f
padzak <mrcn.pajak@gmail.com>
2023-08-05 17:33:21 +0200

    Added responsive product amount in categories

--- components/home/category/index.js
+++ components/home/category/index.js
@@ -3,7 +3,9 @@ import { BsArrowRightCircle } from 'react-icons/bs';
 import { useMediaQuery } from "react-responsive";
 
 export default function Category({ header, products, background }) {
+    const isSmall = useMediaQuery({ query: "(max-width: 600px)" });
     const isMedium = useMediaQuery({ query: "(max-width: 1300px)" });
+    const slices = isSmall ? 2 : isMedium ? 4 : 6;
     return (
         <div className={styles.category} style={{ background: `${background}`}}>
             <div className={styles.category__header}>
@@ -12,7 +14,7 @@ export default function Category({ header, products, background }) {
             </div>
             <div className={styles.category__products}>
                 {
-                    products.slice(0, isMedium ? 4 : 6).map((product, index) => (
+                    products.slice(0, slices).map((product, index) => (
                         // eslint-disable-next-line react/jsx-key
                         <div className={styles.product}>
                             <img src={product.image} /*alt={product.name}*/ alt="" />
