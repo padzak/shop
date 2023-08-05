086e5aebeb2c370f5f884d1f2d623f34206eb884
padzak <mrcn.pajak@gmail.com>
2023-08-05 19:46:45 +0200

    Added Category model

---
+++ models/Category.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const { ObjectId } = mongoose.Schema;
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: [2, "must be at least 2 characters"],
+        maxLength: [32, "must be at most 32 characters"],
+    },
+    slug: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        index: true,
+    },
+},
+{ 
+    timestamps: true 
+});
+
+const Category = mongoose.models.Category || mongoose.model("Category", categorySchema);
+export default Category;
