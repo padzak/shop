9ba4217defebde3dff460621913376fc72885079
padzak <mrcn.pajak@gmail.com>
2023-08-07 08:25:41 +0200

    Added properties polling for product page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,5 +1,6 @@
 import styles from '../../styles/product.module.scss'
 import db from '../../utils/db'
+import Product from '@/models/Product';
 
 export default function product() {
     return (
@@ -13,7 +14,33 @@ export async function getServerSideProps(context) {
     const style = query.style;
     const size = query.size || 0;
 
+console.log("product", slug, style, size);
+    db.connectDb();
+    // ------------
+    let product = await Product.findOne({ slug }).lean();
+    let subProduct = product.subProducts[style];
+    let prices = subProduct?.sizes
+    .map((size) => {
+        return size.price;
+    })
+    .sort((a, b) => {
+        return a - b;
+    });
 
+    console.log("prices", prices);
+    let newProduct = {
+        ...product,
+        images: subProduct.images,
+        sizes: subProduct.sizes,
+        discount: subProduct.discount,
+        sku: subProduct.sku,
+        colors: product.subProducts.map((subProduct) => {
+            return subProduct.color;
+        }),
+    };
+
+    // ------------
+    db.disconnectDb();
 
     return {
         props: {
