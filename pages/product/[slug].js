d0d90e860f1556d85a34bdbf24b930d16f7d9d62
padzak <mrcn.pajak@gmail.com>
2023-08-08 11:39:15 +0200

    Added new product object creation based on the database entry

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -26,19 +26,23 @@ console.log("product", slug, style, size);
     .sort((a, b) => {
         return a - b;
     });
-
-    console.log("prices", prices);
     let newProduct = {
         ...product,
-        images: subProduct.images,
-        sizes: subProduct.sizes,
-        discount: subProduct.discount,
+        images: subProduct?.images,
+        sizes: subProduct?.sizes,
+        discount: subProduct?.discount,
         sku: subProduct.sku,
         colors: product.subProducts.map((subProduct) => {
             return subProduct.color;
         }),
+        priceRange: prices?.length > 1
+        ? `From ${prices[0]} to ${prices[prices.length - 1]}`
+        : '',
+        price: subProduct.discount > 0
+        ? (subProduct.sizes[size].price - (subProduct.sizes[size].price * subProduct.discount / 100)).toFixed(2)
+        : subProduct.sizes[size].price.toFixed(2),
+        priceBefore: subProduct.sizes[size].price,
     };
-
     // ------------
     db.disconnectDb();
 
