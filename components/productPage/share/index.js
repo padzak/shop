83432704ef633d3f605c39252abe86c3dc53fe6c
padzak <mrcn.pajak@gmail.com>
2023-08-18 09:48:11 +0200

    Added facebook share to share component

--- components/productPage/share/index.js
+++ components/productPage/share/index.js
@@ -1,7 +1,12 @@
 import styles from './styles.module.scss';
-
+import { FacebookIcon, FacebookShareButton } from 'react-share';
 export default function Share() {
+    console.log("window", window?.location.href)
     return (    
-        <div>Share</div>
+        <div className={styles.share}>
+            <FacebookShareButton url={window?.location.href}>
+                <FacebookIcon size={38} round={true} />
+            </FacebookShareButton>
+        </div>
     );
 }
