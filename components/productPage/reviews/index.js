ac96149942cb6756d0125c452ff03ea1592f46b1
padzak <mrcn.pajak@gmail.com>
2023-08-21 12:01:04 +0200

    Added rating to Reviews product page component

--- components/productPage/reviews/index.js
+++ components/productPage/reviews/index.js
@@ -1,7 +1,28 @@
+import { Rating } from '@mui/material';
 import styles from './styles.module.scss';
 
 export default function Reviews({ product }) {
     return (
-        <div>Reviews</div>
+        <div className={styles.reviews}>
+            <div className={styles.reviews__container}>
+                <h1>Customer Reviews ({ product.reviews.length })</h1>
+                <div className={styles.reviews__rating}>
+                    <div className={styles.reviews__stats_overview}>
+                        <span>Average Rating</span>
+                        <div className={styles.reviews__stats_overview_rating}></div>
+                        <Rating 
+                            name="half-rating-read"
+                            defaultValue={product.rating}
+                            precision={0.5}
+                            readOnly
+                            style={{
+                                color: "#FACF19",
+                            }}
+                        />
+                        {product.rating == 0 ? "No reviews yet" : product.rating}
+                    </div>
+                </div>
+            </div>
+        </div>
     );
-} 
+}
