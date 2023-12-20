bef7d0554a3cf598da81ba8853f871060cebbdbd
padzak <mrcn.pajak@gmail.com>
2023-12-20 18:36:08 +0100

    Passed reviews to TableHeader component and added ratings

--- components/productPage/reviews/TableHeader.js
+++ components/productPage/reviews/TableHeader.js
@@ -1,11 +1,41 @@
 import styles from "./styles.module.scss";
 import TableSelect from "./TableSelect";
+import { useState } from "react";
 
-export default function TableHeader() {
-    
+export default function TableHeader({ reviews }) {
+    const [rating, setRating] = useState();
+
     return (
         <div className={styles.table__header}>
-
-      </div>  
+        <TableSelect
+            property={rating}
+            text="Rating"
+            data={ratings.filter((x) => x.value !== rating)}
+            handleChange={setRating}
+        />
+        </div>  
     );
 }
+
+const ratings = [
+    {
+        text: "5 star",
+        value: 5,
+    },
+    {
+        text: "4 star",
+        value: 4,
+    },
+    {
+        text: "3 star",
+        value: 3,
+    },
+    {
+        text: "2 star",
+        value: 2,
+    },
+    {
+        text: "1 star",
+        value: 1,
+    },
+];
