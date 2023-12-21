1cf254929344a6b34b729270cb939acf8fba863f
padzak <mrcn.pajak@gmail.com>
2023-12-21 20:54:05 +0100

    Updated cart module

--- pages/cart.js
+++ pages/cart.js
@@ -1,11 +1,23 @@
-import Header from "../components/header";
+import Header from "../components/cart/header";
+import styles from "../styles/cart.module.scss";
 
 export default function Cart() {
+    const cart = [];
   return (
     <>
       <div>
         <Header country="" />
-        cart
+        <div className={styles.cart}>
+            <div className={styles.cart__container}>
+            {
+                cart.length > 1 ? (
+                    <div className={styles.cart__container}></div>
+                ) : (
+                    <div className={styles.empty}>empty</div>
+                )
+            }
+            </div>
+        </div>
       </div>
     </>
   );
