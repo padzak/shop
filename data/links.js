ef5485862fb0605e6fe35af27a368bdc3b117c6f
padzak <mrcn.pajak@gmail.com>
2023-12-21 21:09:39 +0100

    added a link for empty cart img

--- data/links.js
+++ data/links.js
@@ -1,1 +1,2 @@
 export const logo = "https://global-uploads.webflow.com/5e157547d6f791d34ea4e2bf/6087f2b060c7a92408bac811_logo.svg";
+export const emptyCart = "https://cdn3.iconfinder.com/data/icons/shopping-and-ecommerce-29/90/empty_cart-1024.png";
