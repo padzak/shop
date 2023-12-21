27fc906f1d18d363cb5152168c33c460801388fb
padzak <mrcn.pajak@gmail.com>
2023-12-21 20:55:05 +0100

    moved cart header styles

--- components/cart/header/index.js
+++ components/cart/header/index.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "../styles.module.scss";
+import styles from "./styles.module.scss";
 import { logo } from '@/data/links';
 import { MdPlayArrow } from "react-icons/md";
 
