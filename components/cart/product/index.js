880190a7abace38c3ae3c757b86d425b68b212fa
padzak <mrcn.pajak@gmail.com>
2023-12-27 20:03:15 +0100

    Added product cart component

---
+++ components/cart/product/index.js
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss";
+
+export default function Product() {
+    return (
+        <>
+            <div>
+                
+            </div>
+        </>
+    )
+}
