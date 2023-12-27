5e237c1aa43a895b08d3678544689c38cf668b04
padzak <mrcn.pajak@gmail.com>
2023-12-27 20:16:50 +0100

    further development of cart page

--- components/cart/product/index.js
+++ components/cart/product/index.js
@@ -1,10 +1,14 @@
 import styles from "./styles.module.scss";
-
-export default function Product() {
+import { storeImg } from "@/data/links";
+export default function Product({ product }) {
     return (
         <>
-            <div>
-                Product
+            <div className={`${styles.card} ${styles.product}`}>
+                {product.quantity < 1 && <div className={styles.blur}></div>}
+                <div className={styles.product__header}>
+                    <img src={storeImg} alt="store" />
+                    OFFICIAL STORE
+                </div>
             </div>
         </>
     )
--- data/links.js
+++ data/links.js
@@ -1,2 +1,3 @@
 export const logo = "https://global-uploads.webflow.com/5e157547d6f791d34ea4e2bf/6087f2b060c7a92408bac811_logo.svg";
 export const emptyCart = "https://cdn3.iconfinder.com/data/icons/shopping-and-ecommerce-29/90/empty_cart-1024.png";
+export const storeImg = "https://cdn.vectorstock.com/i/1000x1000/01/24/store-icon-vector-23190124.webp"
