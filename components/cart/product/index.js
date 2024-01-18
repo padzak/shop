27279333428c9b94d6f473f10bf29d5606624dae
padzak <mrcn.pajak@gmail.com>
2024-01-18 20:52:18 +0100

    Added grid for shopping cart items

--- components/cart/product/index.js
+++ components/cart/product/index.js
@@ -1,5 +1,8 @@
 import styles from "./styles.module.scss";
 import { storeImg } from "@/data/links";
+import { AiOutlineDelete } from "react-icons/ai";
+import { BsHeart } from "react-icons/bs";
+
 export default function Product({ product }) {
     return (
         <>
@@ -8,6 +11,26 @@ export default function Product({ product }) {
                 <div className={styles.product__header}>
                     <img src={storeImg} alt="store" />
                     OFFICIAL STORE
+                </div>
+                <div className={styles.product__image}>
+                    <div className={styles.checkbox}></div>
+                    <img src={product.images[0].url} alt=""/>
+                    <div className={styles.col}>
+                        <div className={styles.grid}>
+                            <h1>
+                                {
+                                    product.name.length > 30 ? `${product.name.substring(0, 30)}...` : product.name
+                                }
+                            </h1>
+                            <div style={{ zIndex:'2'}}>
+                                <BsHeart />
+                            </div>
+                            <div style={{ zIndex:'2'}}>
+                                <AiOutlineDelete />
+                            </div>
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
         </>
