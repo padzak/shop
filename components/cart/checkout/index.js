09390d73aef71fc2d30cf68adf9a0b6ed4420084
padzak <mrcn.pajak@gmail.com>
2024-01-20 11:32:56 +0100

    added cart checkout section

--- components/cart/checkout/index.js
+++ components/cart/checkout/index.js
@@ -1,11 +1,26 @@
 import styles from "./styles.module.scss";
 
 export default function Checkout({ subtotal, shippingFee, total, selected }) {
-    return(
-        <>
-            <div className={`${styles.cart__checkout} ${styles.card}`}>
-                <h2>Order summary</h2>
-            </div>  
-        </>
-    );
+  return (
+    <>
+      <div className={`${styles.cart__checkout} ${styles.card}`}>
+        <h2>Order summary</h2>
+        <div className={styles.cart__checkout_line}>
+          <span>Subtotal</span>
+          <span>{subtotal} PLN</span>
+        </div>
+        <div className={styles.cart__checkout_line}>
+          <span>Shipping</span>
+          <span>{shippingFee} PLN</span>
+        </div>
+        <div className={styles.cart__checkout_line}>
+          <span>Total</span>
+          <span>{total} PLN</span>
+        </div>
+        <div className={styles.submit}>
+            <button className={styles.submit__button} disabled={!selected}>Checkout</button>
+        </div>  
+      </div>
+    </>
+  );
 }
