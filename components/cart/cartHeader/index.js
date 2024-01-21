ce4139b3c86a5e73eb23ad9ffb126c81f8f8289f
padzak <mrcn.pajak@gmail.com>
2024-01-21 23:29:29 +0100

    Added handle select all cart items

--- components/cart/cartHeader/index.js
+++ components/cart/cartHeader/index.js
@@ -8,10 +8,18 @@ export default function CartHeader({ cartItems, selected, setSelected }) {
       const check = JSON.stringify(cartItems) === JSON.stringify(selected);
       setActive(check);
     }, [cartItems, selected]);
+    const handleSelect = () => {
+        if (selected.length !== cartItems.length) {
+            setSelected(cartItems);
+        }
+        else {
+            setSelected([]);
+        }
+    };
     return <div className={`${styles.cart__header} ${styles.card}`}>
         <h1>Item Summary({ cartItems.length })</h1>
         <div className={styles.flex}>
-            <div className={`${styles.checkbox} ${active ? styles.active : ""}`}>
+            <div className={`${styles.checkbox} ${active ? styles.active : ""}`} onClick={() => handleSelect()}>
                 <span>Select all items</span>
             </div>
         </div>
