2972e40ee7e44807462ffbdaec69ab4bb72f0efb
padzak <mrcn.pajak@gmail.com>
2024-01-21 23:39:33 +0100

    Added utility for comparing json arrays

--- components/cart/cartHeader/index.js
+++ components/cart/cartHeader/index.js
@@ -1,11 +1,13 @@
 import styles from "./styles.module.scss";
 import { useState, useEffect } from "react";
+import { compareArrays } from "@/utils/arrays";
+
 // TODO there is something wrong with the header on the cart page. It is covered for some reason
 
 export default function CartHeader({ cartItems, selected, setSelected }) {
     const [active, setActive] = useState(false);
     useEffect(() => {
-      const check = JSON.stringify(cartItems) === JSON.stringify(selected);
+      const check = compareArrays(cartItems, selected);
       setActive(check);
     }, [cartItems, selected]);
     const handleSelect = () => {
