4a24395e0cc80cecaad8eed404ce86beebc10245
padzak <mrcn.pajak@gmail.com>
2024-01-22 21:56:20 +0100

    added images and links for payment methods

--- data/links.js
+++ data/links.js
@@ -1,3 +1,7 @@
 export const logo = "https://global-uploads.webflow.com/5e157547d6f791d34ea4e2bf/6087f2b060c7a92408bac811_logo.svg";
 export const emptyCart = "https://cdn3.iconfinder.com/data/icons/shopping-and-ecommerce-29/90/empty_cart-1024.png";
-export const storeImg = "https://cdn.vectorstock.com/i/1000x1000/01/24/store-icon-vector-23190124.webp"
+export const storeImg = "https://cdn.vectorstock.com/i/1000x1000/01/24/store-icon-vector-23190124.webp";
+export const visa = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Visa_Inc._logo.svg/2560px-Visa_Inc._logo.svg.png";
+export const mastercard = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Mastercard-logo.svg/2560px-Mastercard-logo.svg.png";
+export const blik = "https://pep.pl/online/wp-content/uploads/sites/2/2022/04/BLIK-LOGO-RGB.png";
+export const buyerProtection = "https://i.pinimg.com/originals/e0/ae/95/e0ae959ebb1255116b69f8c11c1efbd5.jpg";
