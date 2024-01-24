7abc9bc1569c8d79bdf875278749408f74b880ac
padzak <mrcn.pajak@gmail.com>
2024-01-24 18:18:12 +0100

    Added Cart database schema

---
+++ models/Cart.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const { ObjectId } = mongoose.Schema;
+const cartSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        product: {
+          type: ObjectId,
+          ref: "Product", // reference to Product model
+        },
+        name: {
+          type: String,
+        },
+        image: {
+          type: String,
+        },
+        size: {
+          type: String,
+        },
+        style: {
+          style: String,
+          color: String,
+          image: String,
+        },
+        qty: {
+          type: Number,
+        },
+        color: {
+          color: String,
+          image: String,
+        },
+        price: {
+          type: Number,
+        },
+      },
+    ],
+    cartTotal: Number,
+    totalAfterDiscount: Number,
+    user: {
+      type: ObjectId,
+      ref: "User", // reference to User model
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Cart = mongoose.models.Cart
+  ? mongoose.models.Cart
+  : mongoose.model("Category", cartSchema);
+export default Cart;
