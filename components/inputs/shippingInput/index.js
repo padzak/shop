33a29c7771a582d5c9a0a6283c1c21084fa0b9ac
padzak <mrcn.pajak@gmail.com>
2024-01-29 20:44:39 +0100

    shipping input modification

--- components/inputs/shippingInput/index.js
+++ components/inputs/shippingInput/index.js
@@ -1,5 +1,20 @@
 import styles from "./styles.module.scss";
+import { useField, ErrorMessage } from "formik";
+import { useEffect, useState, useRef } from "react";
 
-export default function ShippingInput() {
-    return <div>ShippingInput</div>;
+export default function ShippingInput({ placeholder, ...props }) {
+  const [field, meta] = useField(props);
+  return (
+    <div
+      className={`${styles.input} ${
+        meta.touched && meta.error && styles.error
+      }`}
+    >
+      <div className={styles.input__wrapper}>
+        <input type={field.type} name={field.name} {...field} {...props} />
+        <span>{placeholder}</span>
+        <p>{meta.touched && meta.error && <ErrorMessage name={field.name} />}</p>
+      </div>
+    </div>
+  );
 }
