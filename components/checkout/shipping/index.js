2b6e869854e6cbe798c140caa3207f92bdf05cbb
padzak <mrcn.pajak@gmail.com>
2024-01-29 20:09:13 +0100

    added further form validation

--- components/checkout/shipping/index.js
+++ components/checkout/shipping/index.js
@@ -2,6 +2,7 @@ import styles from "./styles.module.scss";
 import { useState } from "react";
 import { Form, Formik } from "formik";
 import * as Yup from "yup";
+// import "yup-phone";
 
 const initialValues = {
   firstName: "",
@@ -14,6 +15,7 @@ const initialValues = {
   address2: "",
   country: "",
 };
+const phoneRegExp = /^[0-9]{10}$/;
 
 export default function Shipping({
   selectedAddress,
@@ -35,13 +37,39 @@ export default function Shipping({
   } = shipping;
   const validate = Yup.object({
     firstName: Yup.string()
-      .required("Firt name is required")
+      .required("First name is required")
       .min(3, "First name must be at least 3 characters")
       .max(20, "First name must be at most 20 characters"),
     lastName: Yup.string()
       .required("Last name is required")
       .min(3, "Last name must be at least 3 characters")
       .max(20, "Last name must be at most 20 characters"),
+    phoneNumber: Yup.string()
+      .required("Phone number is required.")
+      .matches(phoneRegExp, "Invalid phone number format")
+    //   .phone()
+      .min(3, "Phone number must be at least 3 characters long.")
+      .max(30, "Phone number must be less than 20 characters long."),
+    state: Yup.string()
+      .required("State name is required.")
+      .min(2, "State name should contain 2-60 characters..")
+      .max(60, "State name should contain 2-60 characters."),
+    city: Yup.string()
+      .required("City name is required.")
+      .min(2, "City name should contain 2-60 characters.")
+      .max(60, "City name should contain 2-60 characters."),
+    zipCode: Yup.string()
+      .required("ZipCode/Postal is required.")
+      .min(2, "ZipCode/Postal should contain 2-30 characters..")
+      .max(30, "ZipCode/Postal should contain 2-30 characters."),
+    address1: Yup.string()
+      .required("Address Line 1 is required.")
+      .min(5, "Address Line 1 should contain 5-100 characters.")
+      .max(100, "Address Line 1 should contain 5-100 characters."),
+    address2: Yup.string()
+      .min(5, "Address Line 2 should contain 5-100 characters.")
+      .max(100, "Address Line 2 should contain 5-100 characters."),
+    country: Yup.string().required("Country name is required."),
   });
 
   return (
