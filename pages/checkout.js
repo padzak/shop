b2d4992e38e628cbc6cbdcaaeb78ef5baa730124
padzak <mrcn.pajak@gmail.com>
2024-01-29 19:18:09 +0100

    added checkout page

---
+++ pages/checkout.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../styles/checkout.module.css";
+
+export default function checkout() {
+    return (
+        <div>
+            <h1>Checkout</h1>
+        </div>
+    )
+}
