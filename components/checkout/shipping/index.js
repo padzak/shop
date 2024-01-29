364aa0cf8f29bc2de4ca5a2c7c7ff6e8c9fd3b5c
padzak <mrcn.pajak@gmail.com>
2024-01-29 20:00:19 +0100

    started preparing validation scheme for address form

--- components/checkout/shipping/index.js
+++ components/checkout/shipping/index.js
@@ -1,6 +1,7 @@
 import styles from "./styles.module.scss";
 import { useState } from "react";
 import { Form, Formik } from "formik";
+import * as Yup from "yup";
 
 const initialValues = {
   firstName: "",
@@ -32,6 +33,16 @@ export default function Shipping({
     address2,
     country,
   } = shipping;
+  const validate = Yup.object({
+    firstName: Yup.string()
+      .required("Firt name is required")
+      .min(3, "First name must be at least 3 characters")
+      .max(20, "First name must be at most 20 characters"),
+    lastName: Yup.string()
+      .required("Last name is required")
+      .min(3, "Last name must be at least 3 characters")
+      .max(20, "Last name must be at most 20 characters"),
+  });
 
   return (
     <div className={styles.shipping}>
@@ -48,6 +59,7 @@ export default function Shipping({
           address2: "",
           country: "",
         }}
+        validationSchema={validate}
       >
         {(formik) => (
           <Form>
