67eabf5e1237dd35b6dc4b507992aca37ceb7a18
padzak <mrcn.pajak@gmail.com>
2024-01-29 20:57:15 +0100

    Added input handling for shipping input

--- components/inputs/shippingInput/index.js
+++ components/inputs/shippingInput/index.js
@@ -3,17 +3,35 @@ import { useField, ErrorMessage } from "formik";
 import { useEffect, useState, useRef } from "react";
 
 export default function ShippingInput({ placeholder, ...props }) {
+    const inputRef = useRef(null);
   const [field, meta] = useField(props);
+  const [move, setMove] = useState(false);
+  useEffect(() => {
+    if (field.value.length > 0) {
+      setMove(true);
+    } else {
+      setMove(false);
+    }
+  }, [field.value]);
   return (
     <div
       className={`${styles.input} ${
         meta.touched && meta.error && styles.error
       }`}
     >
-      <div className={styles.input__wrapper}>
-        <input type={field.type} name={field.name} {...field} {...props} />
-        <span>{placeholder}</span>
-        <p>{meta.touched && meta.error && <ErrorMessage name={field.name} />}</p>
+      <div
+        className={styles.input__wrapper}
+        onFocus={() => setMove(true)}
+        onBlur={() => setMove(field.value.length > 0)}
+      >
+        <input ref={inputRef} type={field.type} name={field.name} {...field} {...props} onClick={() => {
+            inputRef.current.focus();
+            setMove(true);
+        }}/>
+        <span className={move ? styles.move : ""}>{placeholder}</span>
+        <p>
+          {meta.touched && meta.error && <ErrorMessage name={field.name} />}
+        </p>
       </div>
     </div>
   );
