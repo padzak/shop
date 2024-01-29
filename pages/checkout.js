f9ff61f021c369f4f31347810925b40f13737a60
padzak <mrcn.pajak@gmail.com>
2024-01-29 19:33:05 +0100

    Finished fetching cart from server side props

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from "../styles/checkout.module.scss";
 import { getSession } from "next-auth/react";
+import User from "@/models/User";
+import Cart from "@/models/Cart";
+import db from "@/utils/db";
 
 export default function checkout() {
     return (
@@ -11,9 +14,22 @@ export default function checkout() {
 }
 
 export async function getServerSideProps(Context) {
+    db.connectDb();
     const session = await getSession(Context);
+    const user = await User.findById(session.user.id);
+    const cart = await Cart.findOne({ user: user._id });
+    db.disconnectDb();
+
+    if (!cart)
+    {
+        return {
+            redirect: "/cart",
+        }
+    }
 
     return {
-        props: {},
+        props: {
+            cart: JSON.parse(JSON.stringify(cart)),
+        },
     }
 }
