e893047a9ba6f8f3a47a953aedbefe3b04639a5f
padzak <mrcn.pajak@gmail.com>
2024-01-31 19:52:07 +0100

    prepared endpoint for pushing user shipping address to database

--- pages/api/user/saveAddress.js
+++ pages/api/user/saveAddress.js
@@ -11,6 +11,13 @@ router.post(async (req, res) => {
   try {
     db.connectDb();
     const { address, user_id } = req.body;
+    const user = User.findById(user_id);
+    await user.updateOne({
+      $push: {
+        addresses: address,
+      },
+    });
+    res.json(address);
     db.disconnectDb();
   } catch (error) {
     return res.status(500).json({ message: error.message });
