0ce630266a3a1155a289dd9df977c50f46292d4e
padzak <mrcn.pajak@gmail.com>
2024-02-08 18:54:03 +0100

    Added basic product information

--- components/checkout/products/index.js
+++ components/checkout/products/index.js
@@ -17,9 +17,22 @@ export default function Products({ cart }) {
           <div className={styles} key={product._id}>
             <div className={styles.product__img}>
               <img src={product.image} alt={product.name} />
+              <div className={styles.product__infos}>
+                <img src={product.color.image} alt={product.color.color} />
+                <span>{product.size}</span>
+                <span>x{product.qty}</span>
+              </div>
             </div>
-            <div className={styles.product__infos}>
-
+            <div className={styles.product__name}>
+                { product.name.length > 18 ? product.name.slice(0, 18) + "..." : product.name}
+            </div>
+            <div className={styles.product__price}>
+                {
+                    (product.price * product.qty).toLocaleString('en-US', {
+                        style: 'currency',
+                        currency: 'USD'
+                    })
+                }
             </div>
           </div>
         ))}
