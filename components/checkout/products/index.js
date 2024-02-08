27c73ebfea02ac1f2cb5f7d1f36e01dcacc0ba77
padzak <mrcn.pajak@gmail.com>
2024-02-08 18:48:21 +0100

    Stared preparing products info in checkout

--- components/checkout/products/index.js
+++ components/checkout/products/index.js
@@ -1,5 +1,29 @@
+import { FaStaylinked } from "react-icons/fa";
 import styles from "./styles.module.scss";
 
 export default function Products({ cart }) {
-    return <div>Cart</div>
+  return (
+    <div className={styles.products}>
+      <div className={styles.products__header}>
+        <h1>Cart</h1>
+        <span>
+          {cart.products.length == 1
+            ? "1 item"
+            : `${cart.products.length} products`}
+        </span>
+      </div>
+      <div className={FaStaylinked.products__wrap}>
+        {cart.products.map((product) => (
+          <div className={styles} key={product._id}>
+            <div className={styles.product__img}>
+              <img src={product.image} alt={product.name} />
+            </div>
+            <div className={styles.product__infos}>
+
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
