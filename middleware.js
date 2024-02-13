2ad1370f0f1b205a99056aed41793337cbc8235e
padzak <mrcn.pajak@gmail.com>
2024-02-13 19:17:20 +0100

    Added token polling for production environment

--- middleware.js
+++ middleware.js
@@ -1,10 +1,12 @@
+import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function middleware(req, res, next) {
-    const { pathname, origin } = req.nextUrl;
+  const { pathname, origin } = req.nextUrl;
 
-    if (pathname == "/checkout")
-    {
-        return NextResponse.redirect(`${origin}`);
-    }
+  const session = await getToken({ req, secret: process.env.SECRET, secureCookie: process.env.NODE_ENV === "production" });
+
+  if (pathname == "/checkout") {
+    return NextResponse.redirect(`${origin}`);
+  }
 }
