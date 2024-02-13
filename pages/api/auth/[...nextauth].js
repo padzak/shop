3afd8555b51a79b0de060d8d60d26d75c70db7ee
padzak <mrcn.pajak@gmail.com>
2024-02-13 19:48:35 +0100

    added default role for next auth authentication methods callback

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -81,6 +81,7 @@ export default NextAuth({
       let user = await User.findById(token.sub);
       session.user.id = token.sub || user.id.toString();
       session.user.role = user.role || 'user';
+      token.role = user.role || 'user';
       return session;
     }
   },
