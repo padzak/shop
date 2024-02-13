91b524684fa30a8a3ba93f3973e17b6386de9e33
padzak <mrcn.pajak@gmail.com>
2024-02-13 19:32:26 +0100

    solved a mistake in middleware

--- middleware.js
+++ middleware.js
@@ -4,8 +4,13 @@ import { NextResponse } from "next/server";
 export async function middleware(req, res, next) {
   const { pathname, origin } = req.nextUrl;
 
-  const session = await getToken({ req, secret: process.env.SECRET, secureCookie: process.env.NODE_ENV === "production" });
+  const session = await getToken({
+    req,
+    secret: process.env.JWT_SECRET,
+    secureCookie: process.env.NODE_ENV === "production",
+  });
 
+  console.log("see the session", session);
   if (pathname == "/checkout") {
     return NextResponse.redirect(`${origin}`);
   }
