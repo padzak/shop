e31e93e913fba39cf1e0b39b71dc277855236ab3
padzak <mrcn.pajak@gmail.com>
2024-02-13 20:04:11 +0100

    started preparing cloudinary post request

--- pages/api/cloudinary/index.js
+++ pages/api/cloudinary/index.js
@@ -4,16 +4,30 @@ import bodyParser from "body-parser";
 import fileUpload from "express-fileupload";
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 
 const router = createRouter();
+router.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 router.post(async (req, res) => {
-
+  try {
+    let files = req.files;
+  } catch (err) {
+    res.status(500).json({ message: error.message });
+  }
 });
-
 
 export default router.handler();
