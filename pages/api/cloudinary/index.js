1787693e736357df3b8d79425bceed78e83a1c82
padzak <mrcn.pajak@gmail.com>
2024-02-13 20:01:51 +0100

    added cloudinary configuration

--- pages/api/cloudinary/index.js
+++ pages/api/cloudinary/index.js
@@ -3,6 +3,12 @@ import cloudinary from "cloudinary";
 import bodyParser from "body-parser";
 import fileUpload from "express-fileupload";
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+});
+
 const router = createRouter();
 
 router.post(async (req, res) => {
