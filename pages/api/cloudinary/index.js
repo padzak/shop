fed0cf0de1fcb42e47e732803fd2150dd850a860
padzak <mrcn.pajak@gmail.com>
2024-02-16 22:01:29 +0100

    added cloudinary handler and delete request

--- pages/api/cloudinary/index.js
+++ pages/api/cloudinary/index.js
@@ -11,12 +11,13 @@ cloudinary.config({
 });
 
 const router = createRouter();
-router.use(
-  fileUpload({
-    useTempFiles: true,
-  })
-)
-.use(imageMiddleware);
+router
+  .use(
+    fileUpload({
+      useTempFiles: true,
+    })
+  )
+  .use(imageMiddleware);
 
 export const config = {
   api: {
@@ -27,11 +28,49 @@ export const config = {
 router.post(async (req, res) => {
   try {
     let files = Object.values(req.files).flat();
-
+    let images = [];
+    for (const file of files) {
+      const img = await uploadToCloudinaryHandler(file, path);
+      images.push(img);
+      removeTmp(file.tempFilePath);
+    }
     res.status(200).json(files);
   } catch (err) {
     res.status(500).json({ message: error.message });
   }
 });
+
+router.delete(async (req, res) => {
+  let image_id = req.body.public_id;
+  cloudinary.v2.uploader.destroy(image_id, async (err, result) => {
+    if (err) return res.status(400).json({ success: false, err });
+    res.json({ success: true, message: "Deleted image" });
+  });
+});
+
+const uploadToCloudinaryHandler = async (file, path) => {
+  return new Promise((resolve) => {
+    cloudinary.v2.uploader.upload(
+      file.tempFilePath,
+      { folder: path },
+      (err, result) => {
+        if (err) {
+          removeTmp(file.tempFilePath);
+          return res.status(400).json({ message: "Upload failed" });
+        }
+        resolve({
+          public_id: result.public_id,
+          url: result.secure_url,
+        });
+      }
+    );
+  });
+};
+
+const removeTmp = (path) => {
+  fs.unlink(path, (err) => {
+    if (err) throw err;
+  });
+};
 
 export default router.handler();
