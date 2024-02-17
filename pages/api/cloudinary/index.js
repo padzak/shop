75c8b41e3bfaa86d6f306f26795f894637cc3d0c
padzak <mrcn.pajak@gmail.com>
2024-02-17 15:56:28 +0100

    fixed cloudinary file upload

--- pages/api/cloudinary/index.js
+++ pages/api/cloudinary/index.js
@@ -3,6 +3,7 @@ import cloudinary from "cloudinary";
 import bodyParser from "body-parser";
 import fileUpload from "express-fileupload";
 import { imageMiddleware } from "@/middleware/imageMiddleware";
+import fs from "fs";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
@@ -27,6 +28,7 @@ export const config = {
 
 router.post(async (req, res) => {
   try {
+    const { path } = req.body;
     let files = Object.values(req.files).flat();
     let images = [];
     for (const file of files) {
@@ -34,9 +36,9 @@ router.post(async (req, res) => {
       images.push(img);
       removeTmp(file.tempFilePath);
     }
-    res.status(200).json(files);
+    res.status(200).json(images);
   } catch (err) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: err.message });
   }
 });
 
@@ -49,19 +51,21 @@ router.delete(async (req, res) => {
 });
 
 const uploadToCloudinaryHandler = async (file, path) => {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
+    // Add reject to handle errors properly
     cloudinary.v2.uploader.upload(
       file.tempFilePath,
       { folder: path },
-      (err, result) => {
+      (err, cloudinaryRes) => {
         if (err) {
-          removeTmp(file.tempFilePath);
-          return res.status(400).json({ message: "Upload failed" });
+          removeTmp(file.tempFilePath); // Cleanup even on error
+          reject(new Error("Upload failed")); // Reject the promise on error
+        } else {
+          resolve({
+            public_id: cloudinaryRes.public_id,
+            url: cloudinaryRes.secure_url, // Ensure you're using secure_url for HTTPS
+          });
         }
-        resolve({
-          public_id: result.public_id,
-          url: result.secure_url,
-        });
       }
     );
   });
