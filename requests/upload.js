aab0a8eaed79d6ba9c523b70e7a3f33e0e71f08b
padzak <mrcn.pajak@gmail.com>
2024-02-17 16:13:17 +0100

    added uploadImages requst

---
+++ requests/upload.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const uploadImages = async (formData) => {
+    const { data } = await axios.post("/api/cloudinary", formData, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    });
+    return data;
+};
