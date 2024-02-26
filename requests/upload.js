d87af3b9a16c34dd1836fd3bf32fad4225ac12cd
padzak <mrcn.pajak@gmail.com>
2024-02-26 22:03:33 +0100

    updated upload requests

--- requests/upload.js
+++ requests/upload.js
@@ -1,10 +1,28 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const uploadImages = async (formData) => {
+    try {
+    console.log("formData:", formData);
     const { data } = await axios.post("/api/cloudinary", formData, {
-        headers: {
-            "Content-Type": "multipart/form-data",
-        },
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
     });
+
+    console.log("formData:", data);
+
     return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error("Axios error occurred:", error.message);
+      if (error.response) {
+        console.error("Data:", error.response.data);
+        console.error("Status:", error.response.status);
+        console.error("Headers:", error.response.headers);
+      }
+    } else {
+      console.error("Non-Axios error occurred:", error.message);
+    }
+    throw error;
+  }
 };
