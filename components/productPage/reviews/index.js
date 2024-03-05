d6952a5e052ad8ca1e96acd5239081b844fcd37a
padzak <mrcn.pajak@gmail.com>
2024-03-05 22:35:48 +0100

    updated produc pate reviews formatting

--- components/productPage/reviews/index.js
+++ components/productPage/reviews/index.js
@@ -1,72 +1,68 @@
-import { Rating } from '@mui/material';
-import styles from './styles.module.scss';
-import { useSession, signIn } from 'next-auth/react';
-import AddReview from './AddReview';
-import Table from './Table';
+import { Rating } from "@mui/material";
+import styles from "./styles.module.scss";
+import { useSession, signIn } from "next-auth/react";
+import AddReview from "./AddReview";
+import Table from "./Table";
 
 export default function Reviews({ product }) {
-    const { data: session } = useSession();
-    return (
-        <div className={styles.reviews}>
-            <div className={styles.reviews__container}>
-                <h1>Customer Reviews ({ product.reviews.length })</h1>
-                <div className={styles.reviews__stats}>
-                    <div className={styles.reviews__stats_overview}>
-                        <span>Average Rating</span>
-                        <div className={styles.reviews__stats_overview_rating}></div>
-                        <Rating 
-                            name="half-rating-read"
-                            defaultValue={product.rating}
-                            precision={0.5}
-                            readOnly
-                            style={{
-                                color: "#FACF19",
-                            }}
-                        />
-                        {product.rating == 0 ? "No reviews yet" : product.rating}
-                    </div>
-                    <div className={styles.reviews__stats_reviews}>
-                        {
-                            product.ratings.map((rating, index) => (
-                                <div className={styles.reviews__stats_review_rating} key={index}>
-                                    <Rating 
-                                        name="half-rating-read"
-                                        defaultValue={5 - index}
-                                        precision={0.5}
-                                        readOnly
-                                        style={{
-                                            color: "#FACF19",
-                                        }}
-                                    />
-                                    <div className={styles.bar}>
-                                        <div
-                                            className={styles.bar__inner}
-                                            style={{ 
-                                                width: `${rating.percentage}%` 
-                                            }}
-                                        ></div>
-                                    </div>
-                                    <span>{rating.percentage}%</span>
-                                </div>
-                            ))
-                        }
-                    </div>
+  const { data: session } = useSession();
+  return (
+    <div className={styles.reviews}>
+      <div className={styles.reviews__container}>
+        <h1>Customer Reviews ({product.reviews.length})</h1>
+        <div className={styles.reviews__stats}>
+          <div className={styles.reviews__stats_overview}>
+            <span>Average Rating</span>
+            <div className={styles.reviews__stats_overview_rating}></div>
+            <Rating
+              name="half-rating-read"
+              defaultValue={product.rating}
+              precision={0.5}
+              readOnly
+              style={{
+                color: "#FACF19",
+              }}
+            />
+            {product.rating == 0 ? "No reviews yet" : product.rating}
+          </div>
+          <div className={styles.reviews__stats_reviews}>
+            {product.ratings.map((rating, index) => (
+              <div className={styles.reviews__stats_review_rating} key={index}>
+                <Rating
+                  name="half-rating-read"
+                  defaultValue={5 - index}
+                  precision={0.5}
+                  readOnly
+                  style={{
+                    color: "#FACF19",
+                  }}
+                />
+                <div className={styles.bar}>
+                  <div
+                    className={styles.bar__inner}
+                    style={{
+                      width: `${rating.percentage}%`,
+                    }}
+                  ></div>
                 </div>
-                {
-                    session
-                    ? (
-                        <AddReview product={product} />
-                    ) : (
-                        <button
-                            onClick={() => signIn()}
-                            className={styles.login_btn}
-                        >
-                            Login to add a review
-                        </button>
-                    )
-                }
-                <Table reviews={product.reviews} allSizes={product.allSizes} colors={product.colors}/>
-            </div>
+                <span>{rating.percentage}%</span>
+              </div>
+            ))}
+          </div>
         </div>
-    );
+        {session ? (
+          <AddReview product={product} />
+        ) : (
+          <button onClick={() => signIn()} className={styles.login_btn}>
+            Login to add a review
+          </button>
+        )}
+        <Table
+          reviews={product.reviews}
+          allSizes={product.allSizes}
+          colors={product.colors}
+        />
+      </div>
+    </div>
+  );
 }
