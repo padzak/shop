c3b6d6f5e6219ea3a484acc5be0a48096c0959c3
padzak <mrcn.pajak@gmail.com>
2024-03-08 11:27:49 +0100

    improved formatting in User model

--- models/User.js
+++ models/User.js
@@ -1,90 +1,90 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const { ObjectId } = mongoose.Schema;
 
 const userSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: [true, 'Please enter your name'],
-        },
-        email: {
-            type: String,
-            required: [true, 'Please enter your email'],
-            unique: true,
-            trim: true,
-        },
-        password: {
-            type: String,
-            required: [true, 'Please enter your password'],
-        },
-        role: {
-            type: String,
-            default: 'user',
-        },
-        image: {
-            type: String,
-            default: '/images/avatar.png',
-        },
-        emailVerified: {
-            type: Boolean,
-            default: false,
-        },
-        defaultPaymentMethod: {
-            type: String,
-            default: '',
-        },
-        addresses: [
-            {
-                firstName: {
-                    type: String,
-                },
-                lastName: {
-                    type: String,
-                },            
-                firstName: {
-                    type: String,
-                },
-                phoneNumber: {
-                    type: String,
-                },
-                addressFirstLine: {
-                    type: String,
-                },
-                addressSecondLine: {
-                    type: String,
-                },
-                city: {
-                    type: String,
-                },
-                zipCode: {
-                    type: String,
-                },
-                country: {
-                    type: String,
-                },
-                active: {
-                    type: Boolean,
-                    default: false,
-                }
-            }
-        ],
-        wishlist: [
-            {
-                product: {
-                    type: ObjectId,
-                    ref: "Product",
-                },
-                style: {
-                    type: String,
-                },
-            },            
-        ]
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
     },
-    {
-        timestamps: true,
-    }
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    image: {
+      type: String,
+      default: "/images/avatar.png",
+    },
+    emailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    defaultPaymentMethod: {
+      type: String,
+      default: "",
+    },
+    addresses: [
+      {
+        firstName: {
+          type: String,
+        },
+        lastName: {
+          type: String,
+        },
+        firstName: {
+          type: String,
+        },
+        phoneNumber: {
+          type: String,
+        },
+        addressFirstLine: {
+          type: String,
+        },
+        addressSecondLine: {
+          type: String,
+        },
+        city: {
+          type: String,
+        },
+        zipCode: {
+          type: String,
+        },
+        country: {
+          type: String,
+        },
+        active: {
+          type: Boolean,
+          default: false,
+        },
+      },
+    ],
+    wishlist: [
+      {
+        product: {
+          type: ObjectId,
+          ref: "Product",
+        },
+        style: {
+          type: String,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const User = mongoose.models.User || mongoose.model('User', userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default User;
