efc72dc4f47760804a9bb7361d87b6649d878f9a
padzak <mrcn.pajak@gmail.com>
2024-03-10 21:51:36 +0100

    added my addresses profile page

---
+++ pages/profile/address.js
@@ -0,0 +1,42 @@
+import { getSession } from "next-auth/react";
+import Layout from "../../components/profile/layout";
+import User from "../../models/User";
+import Shipping from "../../components/checkout/shipping";
+import styles from "../../styles/profile.module.scss";
+import { useState } from "react";
+
+export default function Addresses({ user, tab }) {
+  const [addresses, setAddresses] = useState(user.address.addresses);
+  console.log("ADRES MORDO", user)
+  return (
+    <Layout session={user.user} tab={tab}>
+      <div className={styles.header}>
+        <h1>MY ADDRESSES</h1>
+      </div>
+      <Shipping
+        user={user}
+        addresses={addresses}
+        setAddresses={setAddresses}
+        profile
+      />
+    </Layout>
+  );
+}
+
+export async function getServerSideProps(ctx) {
+  const { query, req } = ctx;
+  const session = await getSession({ req });
+  const tab = query.tab || 0;
+  //--------------
+  const address = await User.findById(session.user.id).select("addresses").lean();
+
+  return {
+    props: {
+      user: {
+        user: session.user,
+        addresses: JSON.parse(JSON.stringify(address)),
+      },
+      tab,
+    },
+  };
+}
