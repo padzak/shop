ef549f44d9ea5844d8b05ef32d73e71d03423c5b
padzak <mrcn.pajak@gmail.com>
2024-04-01 20:42:41 +0200

    updated Header Main component to include functional search bar

--- components/header/Main.js
+++ components/header/Main.js
@@ -1,34 +1,53 @@
-import React from 'react';
-import Link from 'next/link';
-import styles from './styles.module.scss';
-import { RiSearch2Line } from 'react-icons/ri';
-import { FaOpencart } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
-import { logo } from '@/data/links';
+import React from "react";
+import Link from "next/link";
+import styles from "./styles.module.scss";
+import { RiSearch2Line } from "react-icons/ri";
+import { FaOpencart } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { logo } from "@/data/links";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
-export default function Main() {
-    const cart = useSelector((state) => ({ ...state }));
-    return (
-        <div className={styles.main}>
-            <div className={styles.main__container}>
-                <Link legacyBehavior href="/">
-                    <a className={styles.logo}>
-                        <img src={logo}/>
-                    </a>
-                </Link>
-                <div className={styles.search}>
-                    <input type="text" placeholder="Search for products, brands and more"/>
-                    <div className={styles.search__icon}>
-                        <RiSearch2Line />
-                    </div>
-                </div>
-                <Link legacyBehavior href="/cart">
-                    <a className={styles.cart}>
-                        <FaOpencart />
-                        <span style={{color:"black"}}>{0}</span>
-                    </a>
-                </Link>
-            </div>
-        </div>
-    )
+export default function Main({ searchHandler}) {
+  const router = useRouter();
+  const [query, setQuery] = useState(router.query.search || "");
+  const cart = useSelector((state) => ({ ...state }));
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (router.pathname !== "/browse") {
+      if (query.length > 1) {
+        router.push(`/browse?search=${query}`);
+      }
+    } else {
+      searchHandler(query);
+    }
+  };
+  return (
+    <div className={styles.main}>
+      <div className={styles.main__container}>
+        <Link legacyBehavior href="/">
+          <a className={styles.logo}>
+            <img src={logo} />
+          </a>
+        </Link>
+        <form onSubmit={(e) => handleSearch(e)} className={styles.search}>
+          <input
+            type="text"
+            placeholder="Search..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button type="submit" className={styles.search__icon}>
+            <RiSearch2Line />
+          </button>
+        </form>
+        <Link legacyBehavior href="/cart">
+          <a className={styles.cart}>
+            <FaOpencart />
+            <span style={{ color: "black" }}>{0}</span>
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
 }
