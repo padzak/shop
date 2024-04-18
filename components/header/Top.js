4d9b3593a84ed51fcf853910551857b8dd52d8bb
padzak <mrcn.pajak@gmail.com>
2024-04-18 23:48:01 +0200

    improved Header/Top formatting

--- components/header/Top.js
+++ components/header/Top.js
@@ -9,65 +9,59 @@ import UserMenu from "./UserMenu";
 import { useSession } from "next-auth/react";
 
 export default function Top({ country }) {
-    const { data: session } = useSession();
-    const [visible, setVisible] = React.useState(false);
-    return (
-        <div className={styles.top}>
-            <div className={styles.top__container}>
-                <div></div>
-                <ul className={styles.top__list}>
-                    <li className={styles.li}>
-                        <img 
-                            src={country.flag}
-                            alt=""
-                        />
-                        <span>{country.name} / EUR</span>
-                    </li>
-                    <li className={styles.li}>
-                        <MdSecurity />
-                        <span>Buyer Protection</span>
-                    </li>
-                    <li className={styles.li}>
-                        <span>Customer Service</span>
-                    </li>
-                    <li className={styles.li}>
-                        <span>Help</span>
-                    </li>
-                    <li className={styles.li}>
-                        <BsSuitHeart />
-                        <Link href="/profile/wishlist" >
-                            <span>Wishlist</span>
-                        </Link>
-                    </li>
-                    <li className={styles.li}
-                        onMouseOver={() => setVisible(true)}
-                        onMouseLeave={() => setVisible(false)}
-                    >
-                        {session ? (
-                            <li>
-                                <div className={styles.flex}>
-                                    <img
-                                        src={session.user.image}
-                                        alt=""
-                                        />
-                                    <span>{session.user.name}</span>
-                                    <RiArrowDropDownFill />
-                                </div>
-                            </li>
-                        ) : (
-                            <li>
-                            <div className={styles.flex}>
-                                <RiAccountPinCircleLine />
-                                <span>Account</span>
-                                <RiArrowDropDownFill />
-                            </div>
-                        </li>
-                        )}
-                        { visible && <UserMenu session={session}/> }
-                    </li>
-
-                </ul>
-            </div>
-        </div>
-    );
+  const { data: session } = useSession();
+  const [visible, setVisible] = React.useState(false);
+  return (
+    <div className={styles.top}>
+      <div className={styles.top__container}>
+        <div></div>
+        <ul className={styles.top__list}>
+          <li className={styles.li}>
+            <img src={country.flag} alt="" />
+            <span>{country.name} / EUR</span>
+          </li>
+          <li className={styles.li}>
+            <MdSecurity />
+            <span>Buyer Protection</span>
+          </li>
+          <li className={styles.li}>
+            <span>Customer Service</span>
+          </li>
+          <li className={styles.li}>
+            <span>Help</span>
+          </li>
+          <li className={styles.li}>
+            <BsSuitHeart />
+            <Link href="/profile/wishlist">
+              <span>Wishlist</span>
+            </Link>
+          </li>
+          <li
+            className={styles.li}
+            onMouseOver={() => setVisible(true)}
+            onMouseLeave={() => setVisible(false)}
+          >
+            {session ? (
+              <li>
+                <div className={styles.flex}>
+                  <img src={session.user.image} alt="" />
+                  <span>{session.user.name}</span>
+                  <RiArrowDropDownFill />
+                </div>
+              </li>
+            ) : (
+              <li>
+                <div className={styles.flex}>
+                  <RiAccountPinCircleLine />
+                  <span>Account</span>
+                  <RiArrowDropDownFill />
+                </div>
+              </li>
+            )}
+            {visible && <UserMenu session={session} />}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
